07362c19c4023227a6ea1230ed9d5e0b01162f82
Daigo Hirbo <daigoab2017@gmail.com>
2023-07-05 18:32:04 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hello from './components/Hello'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <Hello name='Daigo'/>
+      
+    </>
+  )
+}
+
+export default App
